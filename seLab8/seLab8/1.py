6d22d5a9ec17728ee5a8317c07311143a4f5c61a
Clauzar <vanguardeak@hotmail.com>
2016-03-04 18:27:34 +0700

    Finish One

--- seLab8/seLab8/1.py
+++ seLab8/seLab8/1.py
@@ -49,10 +49,37 @@ class Simple_drawing_window1(QWidget):
 
         p.setPen(QColor(255, 127, 0))
         p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 200, 0, 180 * 32)
+        p.drawPie(50, 150, 100, 100, 0, 180 * 32)
 
-        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+        p.drawImage(QRect(300, 100, 320, 320), self.rabbit)
         p.end()
+
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 50, 100), QPoint(100, 200),
+            QPoint(200, 250), QPoint(250, 150),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPolygon([
+            QPoint( 100, 0), QPoint(75, 20),
+            QPoint(50, 50), QPoint(25, 75),
+        ])
+        p.drawImage(QRect(300, 100, 320, 320), self.rabbit)
+        p.end()
+
 
 class Simple_drawing_window3(QWidget):
     def __init__(self, **kwargs):
@@ -73,13 +100,13 @@ class Simple_drawing_window3(QWidget):
         
         p.drawPolygon([QPoint(50,200), QPoint(150,200), QPoint(100,400)])
 
-        p.drawImage(QRect(200,100,320,320), self.rabbit)
+        p.drawImage(QRect(300, 100, 320, 320), self.rabbit)
         p.end()
 
 def main():
     app = QApplication(sys.argv)
 
-    w = Simple_drawing_window1()
+    w = Simple_drawing_window3()
     w.show()
 
     return app.exec_()
