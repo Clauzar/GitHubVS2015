bb8aa970a48357982b924ff7b4e72b656834955f
Clauzar <vanguardeak@hotmail.com>
2016-03-04 18:39:49 +0700

    Finish 01

--- seLab8/seLab8/1.py
+++ seLab8/seLab8/1.py
@@ -82,25 +82,31 @@ class Simple_drawing_window2(QWidget):
 
 
 class Simple_drawing_window3(QWidget):
-    def __init__(self, **kwargs):
-        QWidget.__init__(self,None)
+    def __init__(self):
+        QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QImage("image/frame-11.png")
+        self.rabbit = QImage("images/frame-11.png")
 
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
 
-        p.setPen(QColor(0, 127, 0))
-        p.drawPolygon([QPoint(70,100), QPoint(100,110), QPoint(130, 100) , QPoint(100, 150)])
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
+        ])
 
-        p.setPen(QColor(225,127,0))
-        p.setBrush(QColor(225,127,0))
-        p.drawPie(50,150,100,100,0,180*16)
-        
-        p.drawPolygon([QPoint(50,200), QPoint(150,200), QPoint(100,400)])
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
 
-        p.drawImage(QRect(300, 100, 320, 320), self.rabbit)
+        p.drawPolygon([
+            QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
+        ])
+
+        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
 
 def main():
